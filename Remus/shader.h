769080700f65fc1dafb57d935f994a3c44bda6b7
Connor <connorcarey2005@gmail.com>
2025-05-01 15:19:51 -0500

    Created shader class.

---
+++ Remus/shader.h
@@ -0,0 +1,22 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+
+struct Shader {
+
+	unsigned int ID; // Program ID.
+
+	Shader(const char* vertexPath, const char* fragmentPath); // Constructor.
+
+	void use(); // Use and activate the shader.
+
+	// Utility functions for uniform variables.
+	void setBool(const std::string& name, bool value) const; 
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+};
+
+#endif
